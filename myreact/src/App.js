906fe19e016e505d010ac500b2f4495fdea0af2e
bolendra moran <bolendra10@gmail.com>
2023-02-13 14:56:42 +0530

    react

---
+++ myreact/src/App.js
@@ -0,0 +1,18 @@
+//import logo from './logo.svg';
+import './App.css';
+import User from './User';
+function App() {
+
+  function click(){
+    <alert>Hello Brother</alert>
+  }
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <User/>
+      <button onClick={click}>Click Me</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ myreact/src/User.js
@@ -0,0 +1,24 @@
+// Below is a functional Component
+
+// import React from 'react'
+
+// const User = () => {
+//   return (
+//     <h1>Hello I am writing User component in functional component</h1>
+//   )
+// }
+
+// export default User
+
+// ***************************************************************************************************************************
+
+// Below is a class Component
+
+// In a class component never forget to write render ,
+import React, { Component } from "react";
+
+export default class User extends Component {
+  render() {
+    return <h1> Hello User I am using class component</h1>;
+  }
+}
