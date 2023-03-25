5be6193305a5d654a403eb4b24766ac56e9101b5
bolendra moran <bolendra10@gmail.com>
2023-03-25 11:49:23 +0530

    25thmarch

--- myreact/src/App.js
+++ myreact/src/App.js
@@ -1457,3 +1457,41 @@
 
 // POST METHOD WITH API
 
+// import './App.css';
+// import React,{useEffect,useState} from 'react';
+// function App(){
+//     const [name,setName]=useState("");
+//     const [email,setEmail]=useState("");
+//     const [mobile,setMobile]=useState("");
+
+//     function saveData(){
+//         let data={name,email,mobile}
+//         fetch("http://localhost:4000/todo",{
+//             method:"POST",
+//             headers:{
+//                 'Accept':'application/json',
+//                 'Content-Type':'application/json',
+//             },
+//             body:JSON.stringify(data)
+//         }).then((resp)=>{
+//             resp.json().then((result)=>{
+//                 console.log("result",result)
+//             })
+//         })
+//     }
+//     return (
+//         <div className="App">
+//           <h1>POST API Example </h1>  
+//           <input type="text" name="name" value={name} onChange={(e)=>{setName(e.target.value)}}  /> <br /> <br />
+//           <input type="text" name="email"  value={email} onChange={(e)=>{setEmail(e.target.value)}} /> <br /> <br />
+//           <input type="text" name="mobile"  value={mobile} onChange={(e)=>{setMobile(e.target.value)}}/> <br /> <br />
+//           <button type="button" onClick={saveData} >Save New User</button>
+//         </div>
+//       );
+//     }
+//     export default App;
+
+// ***********************************************************************************************************************
+
+// DELETE METHOD WITH API
+
